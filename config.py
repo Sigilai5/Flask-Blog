7522a5d0fb3d60ab5105a9ed7a57cea0b2197f7f
sigilai <you@example.com>
2018-06-30 14:40:54 +0300

    created gitignore

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     General configuration parent class
     '''
 
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://sigilai:root@localhost/pitch'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://sigilai:root@localhost/blog'
     SECRET_KEY = os.environ.get('SECRET_KEY')
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
 
